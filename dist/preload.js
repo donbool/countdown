2a0509dfc74f739dd0de93415d20c82d4e87b83d
bench <72360880+donbool@users.noreply.github.com>
2025-06-28 03:53:29 -0500

    Delete dist directory

--- dist/preload.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-electron_1.contextBridge.exposeInMainWorld('electronAPI', {
-    getData: () => electron_1.ipcRenderer.invoke('get-data'),
-    saveData: (data) => electron_1.ipcRenderer.invoke('save-data', data),
-    send: (channel, data) => electron_1.ipcRenderer.send(channel, data),
-    on: (channel, func) => electron_1.ipcRenderer.on(channel, (event, ...args) => func(...args)),
-});
